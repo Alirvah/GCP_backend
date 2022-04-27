e29b9d6e0deba41211e02b8d4a6ee258c238ece9
Jaroslav89 <jaroslav.brada@protonmail.com>
2022-04-27 18:11:30 +0000

    ide to kua

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ A sample Hello World server.
 import os
 from flask import Flask, render_template, jsonify
 from flask_cors import CORS
+from flask import request
 
 import numpy as np
 import pandas as pd
 import pickle
 import io
 from sklearn.preprocessing import StandardScaler
+
+
+import glob
 
 # pylint: disable=C0103
 app = Flask(__name__)
@@ -30,10 +34,15 @@ def hello():
         Revision=revision)
 
 
-@app.route('/check')
+@app.route('/check', methods=['POST'])
 def check():
 
-    inputLine="4462,-2.30334956758553,1.759247460267,-0.359744743330052,2.33024305053917,-0.821628328375422,-0.0757875706194599,0.562319782266954,-0.399146578487216,-0.238253367661746,-1.52541162656194,2.03291215755072,-6.56012429505962,0.0229373234890961,-1.47010153611197,-0.698826068579047,-2.28219382856251,-4.78183085597533,-2.61566494476124,-1.33444106667307,-0.430021867171611,-0.294166317554753,-0.932391057274991,0.172726295799422,-0.0873295379700724,-0.156114264651172,-0.542627889040196,0.0395659889264757,-0.153028796529788,239.93"
+    data = request.get_json(force=True)
+
+    inputLine = data['trans']
+    print(inputLine)
+
+    #inputLine="4462,-2.30334956758553,1.759247460267,-0.359744743330052,2.33024305053917,-0.821628328375422,-0.0757875706194599,0.562319782266954,-0.399146578487216,-0.238253367661746,-1.52541162656194,2.03291215755072,-6.56012429505962,0.0229373234890961,-1.47010153611197,-0.698826068579047,-2.28219382856251,-4.78183085597533,-2.61566494476124,-1.33444106667307,-0.430021867171611,-0.294166317554753,-0.932391057274991,0.172726295799422,-0.0873295379700724,-0.156114264651172,-0.542627889040196,0.0395659889264757,-0.153028796529788,239.93"
 
     modelFileName="storedModel.pckl"
 
@@ -45,15 +54,15 @@ def check():
     """
 
     # Create datafile
-    TESTDATA=csvHeader+"\n"+inputLine
+    TESTDATA=csvHeader+"\n"+str(inputLine)
     df = pd.read_csv(io.StringIO(TESTDATA), sep=",")
 
     # Prepare scaler
     sc=StandardScaler()
-    sc.set_params(**{'copy': True, 'with_mean': True, 'with_std': True})
+    sc.set_params(copy=True, with_mean=True, with_std=True)
 
     # Transform ammount column
-    df['normAmount'] = sc.fit_transform(df['Amount'].values.reshape (-1,1))
+    df['normAmount'] = 0 #sc.fit_transform(df['Amount'].values.reshape(-1,1))
 
     # Remove not needed columns
     df = df.drop (['Time', 'Amount'], axis = 1);
